a790923170724298c7e0458e1137b99578e21d15
Donglehae <dongledh@gmail.com>
2021-09-29 23:02:37 +0900

    fix: remark cancel

--- Programmers/Level 1/82612.py
+++ Programmers/Level 1/82612.py
@@ -1,13 +1,13 @@
 # 위클리 챌린지: 1주차_부족한 금액 계산하기
 
-# def solution(price, money, count):
-#   answer = 0
-#   sum = 0
-#   for i in range(count):
-#     sum += price+(price*i)
-#   if sum-money > 0:
-#     answer = sum-money
-#   return answer
+def solution(price, money, count):
+  answer = 0
+  sum = 0
+  for i in range(count):
+    sum += price+(price*i)
+  if sum-money > 0:
+    answer = sum-money
+  return answer
 
 # 다른 풀이
 # def solution(price, money, count):
